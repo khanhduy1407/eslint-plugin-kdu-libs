5ccee73c3efdeea43f2d7ffd634d9c074489f3f1
khanhduy1407 <kn145660@gmail.com>
2022-01-19 08:34:24 +0700

    Upgrade version to 3.0.0

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  plugins: ['html', 'kdu-libs'],
+  extends: ['plugin:kdu/essential'],
+  plugins: ['kdu-libs'],
 
   parserOptions: {
+    parser: require.resolve('babel-eslint'),
     ecmaVersion: 2022,
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      jsx: true
-    },
     sourceType: 'module'
   },
 
@@ -136,6 +134,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'object-curly-spacing': [2, 'always', { objectsInObjects: false }],
     'array-bracket-spacing': [2, 'never'],
-    'kdu-libs/jsx-uses-vars': 2
+    'kdu-libs/jsx-uses-vars': 2,
+    'kdu/require-v-for-key': 0
   }
 }
